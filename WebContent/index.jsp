5da48189770b792df12209c865231335335bf2de
sainath <saigoud1000@gmail.com>
2016-01-13 13:05:46 -0500

    First commit.

---
+++ WebContent/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body style="background-color:yellow" >
+<form action="./welcome.html">
+
+Name : <input type="text" name="name"/>
+<input type="submit" value="say hi!!"/>
+</form>
+
+
+</body>
+</html>
---
+++ src/Controller.java
@@ -0,0 +1,19 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+public class Controller extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String name = request.getParameter("name");
+		ModelAndView mav = new ModelAndView("sucess");
+		mav.addObject("msg","hi" +""+name);
+		return mav;
+	}
+
+}
